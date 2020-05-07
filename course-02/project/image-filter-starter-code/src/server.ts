dbec2278dbc84879cb1aba4664acc6d4b7653d3e
arnaudlemaire <arnaud@geekylab.com>
2020-05-07 21:13:41 +0200

    delete filtered image locally, once served from API response

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import validUrl from 'valid-url';
+import fs from 'fs';
 import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
@@ -14,24 +15,23 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  // @TODO
+  // Deletes any files on the server on finish of the response
+  app.use(function ( req, res, next ) {
+    req.on("end", function() { 
+    
+      console.log(res.locals.filtered_image_url);
+      deleteLocalFiles([res.locals.filtered_image_url]);
 
-  /**************************************************************************** */
+      // let array = [];
+      // fs.readdir(testFolder, (err, files) => {
+      //   files.forEach(file => array.push(`${testFolder}${file}`));
+      // });
 
-  //! END @TODO1
-  
+    });
+    next();
+  });
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
@@ -46,7 +46,7 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
     if ( !validUrl.isUri(image_url) ) { res.status(422).send('Invalid url.'); }
 
     const filtered_image_url = await filterImageFromURL(image_url);
-
+    res.locals.filtered_image_url = filtered_image_url;
     res.sendFile(filtered_image_url);
 
   });
